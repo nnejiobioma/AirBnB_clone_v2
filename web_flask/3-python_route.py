4c3cb9d2494ed886959145ad175f015c5e5b7195
obiomanneji <nneji.obioma@gmail.com>
2023-07-22 10:28:23 -0700

    Third task handled

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This is a Flask web application that:
+1. Listens on 0.0.0.0:5000
+2. Route / displays "Hello HBNB!"
+3. Route /hbnb that displays "HBNB"
+4. Route /c/<text> displays C followed by text
+5. Route /python/<text> that displays Python and text value
+6. Displays "is cool" as default value of text
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def airbnb():
+    """
+    This route / displays "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This route /hbnb displays "HBNB!"
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_print(text):
+    """
+    This route /c/<text> displays C followed by value of text variable
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_print(text="is cool"):
+    """
+    This route /python/<text> displays Python followed by value of text variable
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
