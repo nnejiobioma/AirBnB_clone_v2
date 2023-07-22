3736ea53d8d2b5b435faaf90aa2f7cb8d77482d2
obiomanneji <nneji.obioma@gmail.com>
2023-07-22 10:14:26 -0700

    task two handled

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,15 +4,16 @@ This is a Flask web application that:
 1. Listens on 0.0.0.0:5000
 2. Route / displays "Hello HBNB!"
 3. Route /hbnb that displays "HBNB"
+4. Route /c/<text> displays C followed by text
 """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def airbnb():
     """
-        This Route / displays "Hello HBNB!"
+    This route / displays "Hello HBNB!"
     """
     return 'Hello HBNB!'
 
@@ -20,10 +21,17 @@ def index():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-       This Route /hbnb displays "HBNB!"
+    This route /displays "HBNB!"
     """
     return 'HBNB'
 
+
+@app.route('/c/<text>', strict_slashes=False)
+def cprint(text):
+    """
+    This route /c/<text> displays C followed by value of text variable
+    """
+    return "C {}".format(text.replace('_', ' '))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
