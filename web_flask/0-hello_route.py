e936fbc84e7474b014122f7f04659feadca0e761
obiomanneji <nneji.obioma@gmail.com>
2023-07-22 09:53:15 -0700

    first commit of this project

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a Flask web application that:
+1. Listens on 0.0.0.0:5000
+2. Route o / displays "Hello HBNB!"
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+        Route / displays "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
