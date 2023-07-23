70866e8ba2c32cdc9a795d4be865bd1bbc9f8f32
obiomanneji <nneji.obioma@gmail.com>
2023-07-23 12:37:14 -0700

    state is amended

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" This holds class State for the app"""
+import models
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+from models.base_model import BaseModel, Base
 
+class State(BaseModel, Base):
+    """This is state representation """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    def __init__(self, *args, **kwargs):
+        """state initialization"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """list of city is gathered for instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
